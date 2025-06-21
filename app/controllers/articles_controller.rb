d2d26ba624a611a70ea22f23d3e123e152263642
Victor Monta <montavictor1@gmail.com>
2025-06-21 09:48:18 +0800

    Edit, Update and Destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to @articles_path
+      redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
